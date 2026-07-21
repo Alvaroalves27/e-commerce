56ac6d16894240c823e464e77d76027a833a81e4
alvaro_alves <alvaroalves264@gmail.com>
2026-07-21 19:37:24 -0300

    feat(app): adiciona UpperCasePipe e define nome da loja no componente principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ export const routes: Routes = [
     {
         path:'',
         component: ListaProdutos,
-    }
+    },
     {
         path:'carrinho',
         component: Carrinho,
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 //import { RouterOutlet } from '@angular/router';
-//import { Produto } from './componets/produto/produto';
-import { ListaProdutos } from './feautures/produtos/lista-produtos/lista-produtos';
+//import { RoutlerOutler, RouterLink } from './componets/produto/produto';
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Mercado Alves ';
 }
